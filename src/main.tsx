3c14cda2ad8f8f56d21d8ae1b1a08491866850d9
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-10-01 15:41:32 -0300

    Adding tailwind and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {RouterProvider} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <RouterProvider router={router}/>
-  </StrictMode>,
-)
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,24 @@
+import { Canvas } from "@react-three/fiber";
+import { Vector3 } from "three";
+
 const LandingPage = () => {
-    return <h1>Hello from Landing Page</h1>
-}
+	return (
+		<div className="flex flex-col w-full h-screen">
+			<Canvas
+				camera={{
+					position: new Vector3(6, 2, 3),
+				}}
+				className="absolute h-full w-full"
+			>
+				<mesh>
+					<boxGeometry args={[2, 2, 2]} />
+					<meshPhongMaterial />
+				</mesh>
+				<ambientLight intensity={0.1} />
+				<directionalLight intensity={0.1} />
+			</Canvas>
+		</div>
+	);
+};
 
 export default LandingPage;
