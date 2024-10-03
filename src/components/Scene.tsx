9465a283d06744519913e90306b334f7e34585c3
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-10-03 00:11:18 -0300

    Improve animations

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,13 +1,8 @@
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import {
-	Center,
-	Float,
-	PerformanceMonitor,
-	PerspectiveCamera,
-	Scroll,
-	ScrollControls,
-	Stars,
-} from "@react-three/drei";
+import { MutableRefObject, useLayoutEffect, useRef } from "react";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { Float, PerformanceMonitor, Stars } from "@react-three/drei";
+
+import { animated, useScroll } from "@react-spring/three";
 
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import { TGALoader } from "three/examples/jsm/loaders/TGALoader.js";
@@ -18,16 +13,58 @@ import {
 	Mesh,
 	MeshStandardMaterial,
 	Object3D,
+	PerspectiveCamera,
 	Vector3,
 } from "three";
 
-import { useRef, useState } from "react";
-import Interface from "./Interface.tsx";
-import Menu from "./Navbar.tsx";
+const Camera = ({
+	scrollAreaRef,
+}: {
+	scrollAreaRef: MutableRefObject<HTMLDivElement>;
+}) => {
+	const initialPosition = new Vector3(0, 0, 30);
 
-function Fighter() {
+	const set = useThree(({ set }) => set);
+	const camera = useThree(({ camera }) => camera);
+	const size = useThree(({ size }) => size);
+	const cameraRef = useRef<PerspectiveCamera>(null!);
+
+	useLayoutEffect(() => {
+		cameraRef.current.aspect = size.width / size.height;
+	}, [size]);
+
+	useLayoutEffect(() => {
+		cameraRef.current.updateProjectionMatrix();
+	});
+
+	useLayoutEffect(() => {
+		const oldCam = camera;
+		set(() => ({ camera: cameraRef.current! }));
+		return () => set(() => ({ camera: oldCam }));
+	}, [camera, cameraRef, set]);
+
+	const scroll = useScroll({
+		container: scrollAreaRef,
+	});
+
+	return (
+		<animated.perspectiveCamera
+			ref={cameraRef}
+			position={initialPosition}
+			fov={50}
+			position-y={scroll.scrollYProgress.to(
+				(val) => initialPosition.y - val * 20
+			)}
+		></animated.perspectiveCamera>
+	);
+};
+
+function Fighter({
+	scrollAreaRef,
+}: {
+	scrollAreaRef: MutableRefObject<HTMLDivElement>;
+}) {
 	const model = useLoader(FBXLoader, "3d/fighter/fighter.fbx");
-
 	const texture = useLoader(TGALoader, "3d/fighter/fighter_albedo.tga");
 
 	model.traverse((child: Object3D) => {
@@ -39,117 +76,140 @@ function Fighter() {
 		}
 	});
 
-	return <primitive object={model} scale={0.01} />;
+	const initialPosition = new Vector3(0, -4, 2);
+
+	const scroll = useScroll({
+		container: scrollAreaRef,
+	});
+
+	return (
+		<animated.group
+			position={initialPosition}
+			rotation={new Euler(0.1, -1, -0.1)}
+			position-z={scroll.scrollYProgress.to(
+				(val) => initialPosition.z + val * 20
+			)}
+			position-x={scroll.scrollYProgress.to(
+				(val) => initialPosition.x + val * 20
+			)}
+		>
+			<Float>
+				<primitive object={model} scale={0.01} />
+			</Float>
+		</animated.group>
+	);
 }
 
-function Planet1() {
+const Planet1 = ({
+	scrollAreaRef,
+}: {
+	scrollAreaRef: MutableRefObject<HTMLDivElement>;
+}) => {
 	const model = useLoader(FBXLoader, "3d/planet/planet1.fbx");
-
 	const groupRef = useRef<Group>(null);
 
+	const initialPosition = new Vector3(-80, -30, -80);
+
+	const scroll = useScroll({
+		container: scrollAreaRef,
+	});
+
 	useFrame((_, delta) => {
-		groupRef.current!.rotation.y += 0.2 * delta;
-		groupRef.current!.rotation.z += 0.1 * delta;
+		groupRef.current!.rotation.y += 0.05 * delta;
+		groupRef.current!.rotation.z += 0.05 * delta;
+		groupRef.current!.rotation.x -= 0.05 * delta;
 	});
 
 	return (
-		<group
+		<animated.group
 			ref={groupRef}
-			position={new Vector3(-80, -30, -80)}
+			position={initialPosition}
 			rotation={new Euler(0.1, -1, -0.1)}
-			scale={new Vector3(6, 6, 6)}
+			scale={new Vector3(10, 10, 10)}
+			position-x={scroll.scrollYProgress.to(
+				(val) => initialPosition.x - val * 30
+			)}
+			position-z={scroll.scrollYProgress.to(
+				(val) => initialPosition.z + val * 30
+			)}
 		>
 			<primitive object={model} />;
-		</group>
+		</animated.group>
 	);
-}
+};
 
-function Planet2() {
+function Planet2({
+	scrollAreaRef,
+}: {
+	scrollAreaRef: MutableRefObject<HTMLDivElement>;
+}) {
 	const model = useLoader(FBXLoader, "3d/planet/planet2.fbx");
-
 	const groupRef = useRef<Group>(null);
 
+	const initialPosition = new Vector3(80, 20, -100);
+
+	const scroll = useScroll({
+		container: scrollAreaRef,
+	});
+
 	useFrame((_, delta) => {
-		groupRef.current!.rotation.y += 0.2 * delta;
+		groupRef.current!.rotation.y += 0.04 * delta;
 	});
 
 	return (
-		<group
+		<animated.group
 			ref={groupRef}
-			position={new Vector3(70, 20, -60)}
+			position={initialPosition}
 			rotation={new Euler(0.1, -1, -0.1)}
-			scale={new Vector3(6, 6, 6)}
+			scale={new Vector3(12, 12, 12)}
+			position-x={scroll.scrollYProgress.to(
+				(val) => initialPosition.x + val * 100
+			)}
+			position-z={scroll.scrollYProgress.to(
+				(val) => initialPosition.z - val * 30
+			)}
 		>
 			<primitive object={model} />;
-		</group>
+		</animated.group>
 	);
 }
 
-function Rig() {
-	return useFrame(({ camera, pointer }, delta) => {
-		camera.position
-			.lerp(
-				new Vector3(pointer.x, pointer.y, camera.position.z),
-				0.8 * delta
-			)
-			.clamp(new Vector3(-1.2, -1.2, 30), new Vector3(1.2, 1.2));
-	});
-}
-
-const Scene = () => {
-	const [menuOpened, setMenuOpened] = useState(false);
-
+const Scene = ({
+	scrollAreaRef,
+}: {
+	scrollAreaRef: MutableRefObject<HTMLDivElement>;
+}) => {
 	return (
-		<>
+		<div className="absolute w-screen h-screen z-0">
 			<Canvas shadows={false}>
 				<color attach="background" args={["#272727"]} />
 
-				<PerspectiveCamera
-					makeDefault={true}
-					position={new Vector3(0, 0, 30)}
-				></PerspectiveCamera>
+				<Camera scrollAreaRef={scrollAreaRef} />
 
 				<ambientLight intensity={0.7} />
 				<directionalLight position={[0, 0, 5]} />
 
-				<ScrollControls pages={4} damping={0.1}>
-					<PerformanceMonitor>
-						<group>
-							<Stars
-								radius={100}
-								depth={50}
-								count={500}
-								factor={4}
-								saturation={0}
-								fade
-								speed={2}
-							/>
+				<PerformanceMonitor>
+					<group>
+						<Stars
+							radius={100}
+							depth={50}
+							count={500}
+							factor={4}
+							saturation={0}
+							fade
+							speed={2}
+						/>
 
-							<Center
-								position={new Vector3(0, -4, 2)}
-								rotation={new Euler(0.1, -1, -0.1)}
-							>
-								<Float>
-									<Fighter />
-								</Float>
-							</Center>
+						<Fighter scrollAreaRef={scrollAreaRef} />
+						<Planet1 scrollAreaRef={scrollAreaRef} />
+						<Planet2 scrollAreaRef={scrollAreaRef} />
+					</group>
+				</PerformanceMonitor>
 
-							<group>
-								<Planet1 />
-								<Planet2 />
-							</group>
-						</group>
-						<Rig />
-					</PerformanceMonitor>
-
-					<Scroll html>
-						<Interface />
-					</Scroll>
-				</ScrollControls>
 				{/*<Perf />*/}
 			</Canvas>
-			<Menu menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
-		</>
+		</div>
 	);
 };
 
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,48 @@
 import Scene from "../components/Scene.tsx";
+import { PropsWithChildren, useRef } from "react";
+import Container from "../components/Container.tsx";
+
+const Section = ({ children }: PropsWithChildren) => {
+	return (
+		<section className="h-min-screen w-screen flex flex-col items-center justify-center">
+			{children}
+		</section>
+	);
+};
+
+const HeroSection = () => {
+	return (
+		<Section>
+			<Container>
+				<div className="h-screen flex flex-col justify-between text-white">
+					<div className="flex flex-col items-center gap-5 py-52">
+						<h1 className="bg-indigo-700 text-5xl p-4 font-bold">
+							Hi, I'm Vitor
+						</h1>
+
+						<h2 className="text-4xl p-4 font-light">
+							I'm a fullstack developer
+						</h2>
+					</div>
+				</div>
+			</Container>
+		</Section>
+	);
+};
 
 const LandingPage = () => {
+	const scrollAreaRef = useRef<HTMLDivElement>(null!);
+
 	return (
-		<div className="w-screen h-screen">
-			<Scene />
+		<div className="relative w-screen h-screen pointer-events-auto">
+			<Scene scrollAreaRef={scrollAreaRef} />
+			<div
+				className="relative h-full z-20 overflow-y-auto overflow-x-hidden"
+				ref={scrollAreaRef}
+			>
+				<HeroSection />
+				<HeroSection />
+			</div>
 		</div>
 	);
 };
